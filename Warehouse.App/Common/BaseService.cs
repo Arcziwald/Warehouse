82d131df95d454c2418433b928c44072d53b9397
Artur Mochnia <amochnia@gmail.com>
2024-01-17 16:18:57 +0000

    update BaseService

---
+++ Warehouse.App/Common/BaseService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.App.Common
+{
+    public class BaseService<T> : IService<T> where T : BaseEntity
+    {
+        public List<T> Items { get; set; }
+
+        public BaseService()
+        {
+            Items = new List<T>();
+        }
+
+        public int AddItem(T item)
+        {
+            Items.Add(item);
+            return item.Id;
+        }
+
+        public List<T> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItem(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
