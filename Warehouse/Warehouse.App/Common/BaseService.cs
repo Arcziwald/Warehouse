d0fa647bd9385ec6cb22bb0d4de88f2c51f1c104
Artur Mochnia <amochnia@gmail.com>
2024-01-30 15:47:22 +0000

    refactoring Warehouse.App

---
+++ Warehouse/Warehouse.App/Common/BaseService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Warehouse.App.Abstract;
+using Warehouse.Domain.Common;
+
+namespace Warehouse.App.Common
+{
+    public class BaseService<T> : IService<T> where T : BaseEntity
+    {
+        public List<T> Items { get; set; }
+
+        public BaseService()
+        {
+            Items = new List<T>();
+        }
+
+        public int GetLastId()
+        {
+            if (Items.Any())
+            {
+                return Items.Max(p => p.Id);
+            }
+            return 0;
+        }
+
+        public int AddItem(T item)
+        {
+            Items.Add(item);
+            return item.Id;
+        }
+
+        public List<T> GetAllItems()
+        {
+            return Items;
+        }
+
+        public void RemoveItem(T item)
+        {
+            Items.Remove(item);
+        }
+
+        public int UpdateItem(T item)
+        {
+            var entity = Items.FirstOrDefault(p => p.Id == item.Id);
+            if (entity != null)
+            {
+                // Zaktualizuj właściwości encji
+                // Na przykład: entity.Name = item.Name;
+                // Dalsze aktualizacje właściwości...
+            }
+            return item.Id; // Zwróć ID przekazanego elementu, nawet jeśli nie znajduje się on w liście
+        }
+
+        public T GetItemById(int id)
+        {
+            return Items.FirstOrDefault(p => p.Id == id) ?? default!;
+        }
+    }
+}
---
+++ Warehouse/Warehouse.App/Managers/ItemManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.App.Abstract;
+using Warehouse.App.Concrete;
+using Warehouse.Domain.Entity;
+
+namespace Warehouse.App.Managers
+{
+    public class ItemManager
+    {
+        private readonly MenuActionService _actionService;
+        private IService<Item> _itemService;
+        public ItemManager(MenuActionService actionService, IService<Item> itemService)
+        {
+            _itemService = itemService;
+            _actionService = actionService;
+        }
+        public int AddNewItem()
+        {
+            var addNewItemMenu = _actionService.GetMenuActionsByMenuName("AddNewItemMenu");
+            Console.WriteLine("Please select item type:");
+            foreach (var action in addNewItemMenu)
+            {
+                Console.WriteLine($"{action.Id}. {action.Name}");
+            }
+
+            if (!int.TryParse(Console.ReadLine(), out int typeId))
+            {
+                Console.WriteLine("Invalid input for type. Please enter a valid number.");
+                return -1; // lub inna logika obsługi błędów
+            }
+
+            Console.WriteLine("Please insert name for item:");
+            var name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Item name cannot be empty.");
+                return -1; // lub inna logika obsługi błędów
+            }
+
+            var lastId = _itemService.GetLastId();
+            Item item = new Item(lastId + 1, name, typeId);
+            _itemService.AddItem(item);
+            return item.Id;
+        }
+    }
+}
