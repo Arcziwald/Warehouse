66d0a3666bd11d9516303630b8534737f9dca0aa
Artur Mochnia <amochnia@gmail.com>
2024-01-30 15:47:01 +0000

    refactoring Domain

---
+++ Warehouse/Warehouse.Domain/Entity/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using Warehouse.Domain.Common;
+
+namespace Warehouse.Domain.Entity
+{
+    public class Item : BaseEntity
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Type")]
+        public int TypeId { get; set; }
+        [XmlElement("Quantity")]
+        public int Quantity { get; set; }
+        protected bool isLowInWarehouse;
+
+        public Item()
+        {
+            Name = string.Empty; // Inicjalizacja domyślną pustą wartością
+        }
+        public Item(int id, string name, int typeId)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name)); // Rzucenie wyjątku, jeśli name jest null
+            TypeId = typeId;
+        }
+    }
+}
---
+++ Warehouse/Warehouse.Domain/Entity/MenuAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Domain.Common;
+
+namespace Warehouse.Domain.Entity
+{
+    public class MenuAction : BaseEntity
+    {
+        public string Name { get; set; }
+        public string MenuName { get; set; }
+
+        public MenuAction(int id, string name, string menuName)
+        {
+            Id = id;
+            Name = name;
+            MenuName = menuName;
+        }
+    }
+}
